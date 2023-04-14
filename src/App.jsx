3c90e84253c277d18bd296a972034f9afb7c82ac
Mr.Walker.27 <navdeepsingh99nos@gmail.com>
2023-04-14 12:20:56 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	modified:   src/Nav.css 	modified:   src/components/Home.jsx 	new file:   src/components/Nav.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Story from "./components/Story";
+import Nav from "./components/Nav";
 function App() {
   
   return (
-    <>
+    <><div>{Nav()}</div>
       <Routes>
         <Route
           path="/News-React-api"
